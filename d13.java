94c050b5f94f0eac9c1491ce7e4be9282725d04d
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-02-16 19:01:11 +0400

    Update d13.java

--- d13.java
+++ d13.java
@@ -1,40 +1,40 @@
 import java.util.Scanner;
-public class d13{
-  public static void main(String[] arg){
-    Scanner sc=new Scanner(System.in);
+public class d13 {
+  public static void main(String[] arg) {
+    Scanner sc = new Scanner(System.in);
     String[] phone = new String[3];
-    for (int x=1; x>0; x++){
+    for (int x = 1; x > 0; x++) {
       listMenu();
-      int a=sc.nextInt();
-      if (a==1){
+      int a = sc.nextInt();
+      if (a == 1) {
         weatherIndicators();
       }
-      if (a==2){
+      if (a == 2) {
         calculation(sc);
       }
-      if (a==3){
+      if (a == 3) {
         weekDay();
       }
-      if (a==4){
+      if (a == 4) {
         heightWeight(sc);
       }
-      if (a==5){
+      if (a == 5) {
         break;
       }
-      if (a==6){
- 
-        if (getFreeSpace(phone)!=-1)
-          addNumber(sc, phone); 
-        else
-    System.out.println("No free space found");
+      if (a == 6) {
 
-     }
-      if (a==7){
+        if (getFreeSpace(phone) != -1)
+          addNumber(sc, phone);
+        else
+          System.out.println("No free space found");
+
+      }
+      if (a == 7) {
         showNumber(phone);
       }
     }
   }
-  public static void listMenu(){
+  public static void listMenu() {
     System.out.println("List");
     System.out.println("1. Weather indicators");
     System.out.println("2. Calculator");
@@ -44,91 +44,95 @@ public class d13{
     System.out.println("6. Add a phone number");
     System.out.println("7. Show phone number");
   }
-  public static void weatherIndicators(){
-    int[] array = {12, 15, 10, 6, 9, 20, 10};
-    for (int i:array){
+  public static void weatherIndicators() {
+    int[] array = {
+      12,
+      15,
+      10,
+      6,
+      9,
+      20,
+      10
+    };
+    for (int i: array) {
       System.out.println(i);
     }
   }
 
-  public static void calculation(Scanner sc){
-    int b=sc.nextInt();
-    int c=sc.nextInt();
+  public static void calculation(Scanner sc) {
+    int b = sc.nextInt();
+    int c = sc.nextInt();
     System.out.println("Choose one is operations:");
     System.out.println("1. *");
     System.out.println("2. +");
     System.out.println("3. -");
     System.out.println("4. /");
-    int d=sc.nextInt();
-    if (d==1){
-      int k=b*c;
+    int d = sc.nextInt();
+    if (d == 1) {
+      int k = b * c;
       System.out.println(k);
     }
-    if (d==2){
-      int l=b+c;
+    if (d == 2) {
+      int l = b + c;
       System.out.println(l);
     }
-    if (d==3){
-      int m=b-c;
+    if (d == 3) {
+      int m = b - c;
       System.out.println(m);
     }
-    if (d==4){
-      int p=b/c;
+    if (d == 4) {
+      int p = b / c;
       System.out.println(p);
-    } 
+    }
   }
-  public static void weekDay(){
+  public static void weekDay() {
     System.out.println("Monday");
   }
-  public static void heightWeight(Scanner sc){
+  public static void heightWeight(Scanner sc) {
     System.out.println("Enter height");
-    int d=sc.nextInt();
+    int d = sc.nextInt();
     System.out.println("Enter weight");
-    int f=sc.nextInt();
-    int x=d-f;
-    if (x==100){
+    int f = sc.nextInt();
+    int x = d - f;
+    if (x == 100) {
       System.out.println("Normal weight");
-    }
-    else if (x>100){
-      System.out.println("Underweight "+ ((d-f)-100)+"kq");
-    }
-    else if (x<100){
-      System.out.println("Overweight "+ (100-(d-f))+"kq");
+    } else if (x > 100) {
+      System.out.println("Underweight " + ((d - f) - 100) + "kq");
+    } else if (x < 100) {
+      System.out.println("Overweight " + (100 - (d - f)) + "kq");
     }
   }
 
-  public static void addNumber(Scanner sc, String[] phone){ 
+  public static void addNumber(Scanner sc, String[] phone) {
     System.out.println("Enter the number:");
     sc.nextLine();
-    String num=sc.nextLine();
+    String num = sc.nextLine();
     System.out.println("Number added");
-    int idx=getFreeSpace(phone);
-    if (idx==-1){
+    int idx = getFreeSpace(phone);
+    if (idx == -1) {
       System.out.println("No emptry space found");
-    }
-    else {
-      phone [idx]=num;
-      System.out.println("Number added "+num);
+    } else {
+      phone[idx] = num;
+      System.out.println("Number added " + num);
     }
 
   }
-  public static void showNumber(String[] phone){
-    for (int i=0; i<3; i++)
+  public static void showNumber(String[] phone) {
+    for (int i = 0; i < 3; i++)
       System.out.println(phone[i]);
   }
 
-  public  static int getFreeSpace(String[] phone){
-    int idx=-1;
-    for (int i=0; i<phone.length; i++){
+  public static int getFreeSpace(String[] phone) {
+    int idx = -1;
+    for (int i = 0; i < phone.length; i++) {
 
-      if (phone[i]!=null){
-        if (phone[i].equals("")){
-          idx=1;
+      if (phone[i] != null) {
+        if (phone[i].equals("")) {
+          idx = 1;
           break;
         }
-      }
-      else {
-        idx=i;
+      } else {
+        idx = i;
         break;
       }
     }
@@ -136,5 +140,3 @@ public class d13{
   }
 
 }
-
-
